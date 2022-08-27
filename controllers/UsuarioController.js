8910a324965f49faf3c1daf52a62b480cf419bb4
titomon98 <arturomonterroso3@gmail.com>
2022-08-26 18:19:11 -0600

    Arreglando los modelos

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,12 +1,14 @@
 'user strict'
 const Sequelize = require('sequelize');
-const USR = require('../models/usuarios'); //Llamar al modelo
+//Llamar al modelo
+const db = require("../models");
+const Usuario = db.usuarios;
 
 module.exports = {
     async get(req, res){ //asincronas significa que vamos a estar realizando todo en diferente tiempo
         //y conforme demanda
         const id = req.body.id;
-        await USR.findByPk(id)
+        await Usuario.findByPk(id)
         .then(usuario => res.send(usuario))
         .catch(error => res.status(400).send(error))
     },
@@ -19,7 +21,7 @@ module.exports = {
             estado: form.estado,
         }
 
-        await USR.create(datos).then(usuario =>{
+        await Usuario.create(datos).then(usuario =>{
             res.send(usuario)
         }).catch(err => {
             console.log(err)
@@ -31,7 +33,7 @@ module.exports = {
 
     async update(req, res){
         let form = req.body.form
-        await USR.update({
+        await Usuario.update({
             user: form.user,
             password: form.password,
             estado: form.estado
@@ -43,6 +45,6 @@ module.exports = {
 
     async delete(req, res){
         let form = req.body
-        await USR.destroy({ where: {id: form.id }});
+        await Usuario.destroy({ where: {id: form.id }});
     }
 }
