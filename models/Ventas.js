1d5b8cc037cbe649871217d4cc782a8f71372751
CCaceres1004 <oscar.caceres2321@gmail.com>
2022-08-26 18:01:29 -0600

    Coidificación del Modelo Ventas

---
+++ models/Ventas.js
@@ -0,0 +1,39 @@
+'use strict';
+const sequelize = require("sequelize");
+var _ = require("sequelize");
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class ventas extends Model {
+        static associate(models) {  
+        }
+    };
+    ventas.init({
+        Producto:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Cantidad:{
+            type: DataTypes.INTEGER,
+            allowNull:false
+        },
+        Descripcion:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Precio:{
+            type: DataTypes.DOUBLE,
+            allowNull: false
+        },
+        Vendedor:{
+            type: DataTypes.STRING,
+            allowNull: false 
+        },
+        Fecha:{
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+    });
+    return ventas;
+};
