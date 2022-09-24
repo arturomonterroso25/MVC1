510feba6d4afad38aff4aa812d25ea1b05a6b6ef
CCaceres1004 <oscar.caceres2321@gmail.com>
2022-09-23 18:38:42 -0600

    Modificación del Modelo Empleados para dos o más herencias

--- models/usuarios/Empleados.js
+++ models/usuarios/Empleados.js
@@ -7,8 +7,9 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class empleado extends Model {
     static associate(models) {
-      empleado.belongsTo(models.departamentos,{
+      empleado.belongsTo(models.departamentos,models.sucursal,{
         foreignKey: "ID_Departamento",
+        foreignKey: "ID_Sucursal",
       });
     }
   };
@@ -32,7 +33,11 @@ module.exports = (sequelize, DataTypes) => {
     ID_Departamento: 
     {  
       type: DataTypes.INTEGER, 
-      allowNull: false}
+      allowNull: false},
+      ID_Sucrusal:{
+        type: DataTypes.INTEGER, 
+        allowNull: false
+      },
   }, {
     sequelize,
     modelName: 'empleados',
